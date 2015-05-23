7d60afa0c2d3bc6de52d889c1800cdd1df225bd1
williamthing <williamthing@gmail.com>
2015-05-23 15:50:52 -0700

    hella stuff babygit add .

---
+++ app.js
@@ -0,0 +1,21 @@
+// intializing objects
+var express = require('express');
+var app = express();				
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var path = require('path');
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hjs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// views routes
+var routes = require('./routes/index');
+app.use('/', routes);
+
+// begin server
+server.listen(80, function() {
+	console.log('listening on *:80');
+});
