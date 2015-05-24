4335641144c1a74c68df02a171e265aba5e95845
thomas <toncheng15@hotmail.com>
2015-05-24 02:48:58 -0700

    -check valid input for email

--- public/js/random.js
+++ public/js/random.js
@@ -15,27 +15,28 @@
 		$('#submit').click(function() {
 			//get the email 
 			var email = $('#email_text').val();
+			//check the email
 			if(!email.match("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$")){
 				$('#email_text').val("");
 				$('#email_text').attr('placeholder', "Enter a valid email address");
 			}
-			console.log($('#email_text').val());
-			//get the query
-			var	query = $("#todo_text").val();
-			console.log($("#todo_text").val());
-			//open ajax
-			var ajax = new XMLHttpRequest();
-			ajax.open("POST","/api/query");
-			ajax.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-			ajax.send(JSON.stringify({email:email, query:query}));
-			ajax.onload = function(){
-				var data = JSON.parse(this.responseText);
-				//we found a match
-				if(data.match == 1){
-					var room = data.room_id;
-					window.location.replace("/match");
-				}else{
-					window.location.replace("/no_match");
+			else{
+				//get the query
+				var	query = $("#todo_text").val();
+				//open ajax
+				var ajax = new XMLHttpRequest();
+				ajax.open("POST","/api/query");
+				ajax.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+				ajax.send(JSON.stringify({email:email, query:query}));
+				ajax.onload = function(){
+					var data = JSON.parse(this.responseText);
+					//we found a match
+					if(data.match == 1){
+						var room = data.room_id;
+						window.location.replace("/match");
+					}else{
+						window.location.replace("/no_match");
+					}
 				}
 			}
 		});
